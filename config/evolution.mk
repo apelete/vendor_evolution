7b55fe0fd379e08c7ef93316fd461d9bdfadcaa8
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-28 20:27:05 +0900

    config: properties: Add pihooks default values
    * Baklava QPR2 Beta 1
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/evolution.mk
+++ config/evolution.mk
@@ -1,3 +1,15 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_MODEL?=Pixel 7 Pro \
+    persist.sys.pihooks_FINGERPRINT?=google/cheetah_beta/cheetah:Baklava/BP41.250725.006/13939570:user/release-keys \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP41.250725.006 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-08-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
+
 # Evolution X packages
 PRODUCT_PACKAGES += \
     EvoEgg
