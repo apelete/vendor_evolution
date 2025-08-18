d845342b0dcf3a76468adea1f7716b3a71f3859f
Joey Huab <jhuab@edukasyon.ph>
2025-08-18 12:09:57 +0900

    version: Uprev to 11.1
    * August 2025 patches

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase EVO Version with each major release.
-EVO_VERSION_BASE := 11.0
+EVO_VERSION_BASE := 11.1
 EVO_BUILD_TYPE ?= Unofficial
 
 ifeq ($(WITH_GMS),true)
